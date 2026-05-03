dd54b76df774db76bbcde0fae538989f9135f7ef
hananabdi <abdirahmah2@mymail.nku.edu>
2026-05-02 21:24:09 -0400

    Add functions to display messages on the webpage

---
+++ css/js/script.js
@@ -0,0 +1,8 @@
+function showMessage() {
+  document.getElementById("message").innerHTML = "Welcome to my website!";
+}
+
+function contactMessage() {
+  document.getElementById("contactResult").innerHTML = "Message sent!";
+  return false;
+}
